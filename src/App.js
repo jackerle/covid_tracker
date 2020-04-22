8ac0ea06e9e98630781bc5039cc4c43c54b07820
jackerle <zeustololisis@gmail.com>
2020-04-23 03:51:00 +0700

    added map and marker from latlong file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import MapView from './Component/MapView';
+import 'leaflet/dist/leaflet.css';
+import p_latlong from './LatLong/province.json'
 
 function App() {
+
+  const [province_latlong,setProvince_latlong] = useState(p_latlong);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapView province_latlong={province_latlong}/>
     </div>
   );
 }
---
+++ src/Component/MapView.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Map, TileLayer, Marker, Popup} from 'react-leaflet'
+import {divIcon} from 'leaflet';
+
+
+function MapView(props){
+
+    const {province_latlong} = props
+
+    const markerCreate = Object.keys(province_latlong).map((key,index)=>{
+      //console.log(province_latlong[index]);
+      const {id,province,lat,long} = province_latlong[index]
+      //console.log(id+" "+province)
+      let title = province;
+      return (
+        <Marker
+          key = {`province-${id}`}
+          position = {[lat,long]}
+          icon = {divIcon({className:'marker-icon',iconSize:[16,16]})}
+        >
+          <Popup>{title}</Popup>
+        </Marker>
+      )
+    })
+
+    return(
+        <Map className="map-view" center={[13.7278956,100.52412349999997]} zoom={13}>
+          {console.log(province_latlong)}
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {markerCreate}
+      </Map>
+    );
+}
+
+export default MapView;
