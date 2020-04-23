656f94b8fb41e51f3a7110c3028b352994f90f49
jackerle <zeustololisis@gmail.com>
2020-04-24 00:39:18 +0700

    little fix ui

--- src/Component/Start_modal.js
+++ src/Component/Start_modal.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Start_modal(){
 
     return(
-        <img src='start_modal.jpg' style={{'width':'1200px','height':'650px',}}></img>
+        <img src='start_modal.jpg' style={{'width':'1200px','height':'650px','margin-left':'20px'}}></img>
     )
 }
 
